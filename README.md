87af0bfed44bbd8a19242baa8adda53a3d6075e9
yatchanek <yatsu@gazeta.pl>
2024-08-09 21:24:32 +0200

    Voxel grid cache

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 A simple image-to-voxel converter.
 
-It takes a spritesheet, extracts the data and creates a 3D representation. Animation is also possible.
+It takes a spritesheet, extracts the data and creates a 3D representation. The VoxelSprite can then be animated by changing the anim_frame value via AnimationPlayer (or by code if you prefer it this way).
 
-I tweaked the algorithm a bit and used MultiMesh, dropping the draw call count to 1. But it surely can be optimised further.
+MultiMeshInstance is used to produce the voxels, so the whole VoxelSprite is drawn in one draw call. However, because each voxel is a separate BoxMesh, their number is quite high. The sprite used in this demo uses from 400 to 900 meshes depending on the frame. If I were smarter, I probably could find a way to use some greedy meshing to lump voxels of the same colour. Anyway, on a modern machine, using a couple of VoxelSprites at once shouldn't be a problem.
+
+The latest version cashes all the voxel grids of a spritesheet (a dictionary with coordinates and colour of each voxel) at the beginning, instead of reading the pixels from the sprite region each time the voxels update. I hope it improves the performance a bit.
+
+I'm a noob when it comes to shaders and lightning, so no matter how hard I try, I cannot get the exact same colours as in the spritesheet. Maybe some of you can do it better.
 
 I've included a simple demo scene. Because the sprite I used is not centered, the rotation will get weird if you change the X size of voxels.
 
--- scripts/voxelsprite.gd
+++ scripts/voxelsprite.gd
@@ -15,7 +15,8 @@ class_name VoxelSprite
 @export var fps : int = 10 : 
 	set(value):
 		fps = clamp(value, 1, 30)
-		animation_player.speed_scale = fps / 10.0
+		if is_instance_valid(animation_player):
+			animation_player.speed_scale = fps / 10.0
 		
 @export var voxel_size : Vector3 = Vector3(0.0, 0.0, 0.0) 
 
@@ -25,7 +26,9 @@ var multi_mesh_instance: MultiMeshInstance3D
 
 var animations : PackedStringArray = []
 
-var region_size : Vector2
+var region_size : Vector2i
+
+var pixel_cache_dictionary : Dictionary = {}
 
 var voxel_grid : Dictionary = {}
 
@@ -36,23 +39,19 @@ var current_animation : StringName
 @export var anim_frame : int = 0
 var last_frame : int = 0
 
-var loop_count : int = 0
-
-var elapsed_time : float = 0.0
-
-
 signal animation_finished(_anim_name : StringName)
 
 func _ready() -> void:
-#Creating the multimesh in code to avoid buffer error in Vulkan renderer
 	var mmesh = MultiMesh.new()
+	var voxel_mesh : BoxMesh = BoxMesh.new()
+	voxel_mesh.material = load("res://resources/voxel_shader_material.tres")
+	voxel_mesh.size = voxel_size
+	voxel_mesh.resource_local_to_scene = true
 	mmesh.instance_count = 0
 	mmesh.transform_format = MultiMesh.TRANSFORM_3D
 	mmesh.use_custom_data = true
-	
-	var voxel_mesh : BoxMesh = BoxMesh.new()
-	voxel_mesh.material = load("res://resources/voxel_shader_material.tres")
 	mmesh.mesh = voxel_mesh
+
 	
 	multi_mesh_instance = MultiMeshInstance3D.new()
 	multi_mesh_instance.multimesh = mmesh
@@ -68,19 +67,24 @@ func _ready() -> void:
 	region_size = calculate_region_size()
 
 	create_images()
+	cache_pixels()
 
 	animations = animation_player.get_animation_list()
 	var r : int = animations.find("RESET")
-	animations.remove_at(r)
-	print(animations)
+	if r >= 0:
+		animations.remove_at(r)
+		
 	current_animation = default_animation
 	
+	if current_animation:
+		change_anim_to(current_animation, fps / 10.0)
+		
 	update_voxels()
-	change_anim_to(default_animation)
+
 
 
 #Calculate the size of a single frame
-func calculate_region_size() -> Vector2:
+func calculate_region_size() -> Vector2i:
 	return Vector2(spritesheet.get_width() / h_frames, spritesheet.get_height() / v_frames)
 
 #Change animation to a given one
@@ -88,19 +92,19 @@ func change_anim_to(anim_name : StringName, speed_scale = 1.0):
 	animation_player.speed_scale = speed_scale
 	animation_player.play(anim_name)
 	current_animation = anim_name
-
+	
 func next_anim():
-	if animations.size() <= 1:
+	if animations.size() < 2:
 		return
-	var cur_anim = animations.find(current_animation)
-	change_anim_to(animations[wrapi(cur_anim + 1, 0, animations.size())])
+	var idx : int = animations.find(current_animation)
+	change_anim_to(animations[wrapi(idx + 1, 0, animations.size())])
 
 func prev_anim():
-	if animations.size() <= 1:
+	if animations.size() < 2:
 		return
-	var cur_anim = animations.find(current_animation)
-	change_anim_to(animations[wrapi(cur_anim - 1, 0, animations.size())])
-	
+	var idx : int = animations.find(current_animation)
+	change_anim_to(animations[wrapi(idx - 1, 0, animations.size())])
+
 #Extract individual image data from the spritesheet and put them in an Array
 func create_images():
 	var spritesheet_image = spritesheet.get_image()
@@ -111,34 +115,39 @@ func create_images():
 			
 			frame_images.append(frame_img)
 
-	
+#Creating voxel grid data for each frame
+func cache_pixels():
+	for i in frame_images.size():
+		var img : Image = frame_images[i]
+		pixel_cache_dictionary[i] = get_color_pixels(img)
+		
 #Create the voxel representation of the image
 func update_voxels():
-	var image : Image = frame_images[anim_frame]
-	get_color_pixels(image)
-
+	var grid : Dictionary = pixel_cache_dictionary[anim_frame]
+	
 	multi_mesh_instance.multimesh.instance_count = 0
 	multi_mesh_instance.multimesh.use_custom_data = true
-	multi_mesh_instance.multimesh.mesh.size = voxel_size
-	multi_mesh_instance.multimesh.instance_count = voxel_grid.size()
+	multi_mesh_instance.multimesh.instance_count = grid.size()
 	
 	var instance_count : int = 0
-	for coord in voxel_grid.keys():
+	
+	for coord in grid.keys():
 		var xform : Transform3D = Transform3D(Basis.IDENTITY, Vector3(coord.x * voxel_size.x, -coord.y * voxel_size.y, 0) + Vector3((-region_size.x / 2.0) * voxel_size.x, (region_size.y / 2.0) * voxel_size.y, 0))
 		multi_mesh_instance.multimesh.set_instance_transform(instance_count, xform)
-		multi_mesh_instance.multimesh.set_instance_custom_data(instance_count, voxel_grid[coord])
+		multi_mesh_instance.multimesh.set_instance_custom_data(instance_count, grid[coord])
 		instance_count += 1
 	
 #Filter out fully empty pixels
-func get_color_pixels(img : Image):
-	voxel_grid = {}
+func get_color_pixels(img : Image) -> Dictionary:
+	var grid : Dictionary = {}
 	for y in img.get_height():
 		for x in img.get_width():
 			var pixel_color = img.get_pixel(x, y)
 			if pixel_color.a > 0:
-				voxel_grid[Vector2(x, y)] = pixel_color
+				grid[Vector2(x, y)] = pixel_color
+	return grid
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if last_frame != anim_frame:
 		update_voxels()
 		last_frame = anim_frame
