4af3fc057bfa09fe7e0fe944f5df0a0312128b8c
yatchanek <yatsu@gazeta.pl>
2025-02-09 13:40:34 +0100

    Latest fixes

--- README.md
+++ README.md
@@ -2,19 +2,22 @@ A simple image-to-voxel converter.
 
 It takes a spritesheet, extracts the data and creates a 3D representation. The VoxelSprite can then be animated by changing the anim_frame value via AnimationPlayer (or by code if you prefer it this way).
 
-Thanks to u/DNCGame from Reddit, I switched to vertex animation. First, the VoxelSprite creates two textures: one containing positon offsets of the pixels (only solid ones), the other containing colors of those pixels for each frame. Then, I create ArrayMesh with a number of cubes corresponding to the highest number of pixels in all the frames. The shader reads the positions and colors for each cube, using the UV.x channel. 
-
-I've introduced MultiMesh again instead of creating the mesh in code. It still has more primitives than my previous pure MultiMesh approach, as the maximum number of cubes needed is generated (the included sprite requires about 12500 primitives - not something a modern GPU cannot handle), but moving and coloring the cubes via GPU should be faster than doing it in GDScript for each instance of the MultiMesh. Also, using MultiMesh is much faster than generating the mesh in code.
+The implementation uses Multimesh Instance, so all the voxels can be drawn in a single draw call. The Multimesh and the meshes are created in code instead of the inspector to avoid annoying error messages in the editor about instance count being > 0 and about an empty buffer.
 
 I've included a simple demo scene. 
 
 The spritesheet must be imported as uncompressed (Lossless, VRAM Uncompressed etc.) in order to work.
 
-The VoxelSprite can create textures upon loading, but it's more efficient to provide them befhrehand (in the demo example, it takes about 133ms to load when there are no textures, compared to about 30 with textures).
-I have provided a simple texture creator tool, but you can also use the VoxelSprite itself. As it's a @tool script, if provided with a spritesheet and corretly set h_frames and v_frames, it will generate textures when opened in the editor. You can then save them for later use.
+The VoxelSprite can create textures upon loading, but it's more efficient to provide them beforehand.
+I have provided a simple texture creator tool, but you can also use the VoxelSprite itself - after the first use it will generate the textures. 
+
+*Add the @tool verse at the beginning of the voxelsprite sctipt to make the result visible in the editor.
 
 !!! Sometimes Godot automatically detects that the color/position texture will be used in 3D and sets it to VRAM Compressed. Reimport it as VRAM Uncompressed or Lossless, otherwise it may not display correctly.
 
+Current limitations:
+- No support for transparent pixels.
+- 256x256 px size limit.
 
 Credits:
 Warrior sprite by Clembod.
--- scripts/voxelsprite_multimesh.gd
+++ scripts/voxelsprite_multimesh.gd
@@ -1,6 +1,6 @@
-@tool
 extends Node3D
 class_name VoxelSpriteMultiMesh
+
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var voxel_grid: MultiMeshInstance3D = $VoxelGrid
@@ -21,12 +21,10 @@ class_name VoxelSpriteMultiMesh
 		fps = clamp(value, 1, 30)
 		if is_instance_valid(animation_player):
 			animation_player.speed_scale = fps / 10.0
+			
 @export_category("Other settings")	
-@export var voxel_size : Vector3 = Vector3(0.04, 0.04, 0.08):
-	set(value):
-		if is_instance_valid(voxel_grid):
-			voxel_size = value
-			voxel_grid.material_override.set_shader_parameter("voxel_size", voxel_size)
+@export var voxel_size : Vector3
+
 
 @export var animated : bool = true
 
@@ -41,19 +39,36 @@ var position_offset_scale : float
 @export var anim_frame : int = 0 :
 	set(value):
 		anim_frame = value
-		if is_instance_valid(voxel_grid):
+		if is_instance_valid(voxel_grid) and is_instance_valid(voxel_grid.material_override):
 			voxel_grid.material_override.set_shader_parameter("anim_frame", anim_frame)
 		
+var last_frame : int = 0
+
 var texture_width : int = 0
 
-signal animation_finished(_anim_name : StringName)
+var vertices : PackedVector3Array = []
+var indices : PackedInt32Array = []
+var uvs : PackedVector2Array = []
+var normals : PackedVector3Array = []
+
 
 func _ready() -> void:
 	if !spritesheet or h_frames == 0 or v_frames == 0:
 		return
-
-	region_size = calculate_region_size()
 	
+	#Set Multimesh
+	var mesh : BoxMesh = BoxMesh.new()
+	var mmesh : MultiMesh = MultiMesh.new()
+	mmesh.instance_count = 0
+	mmesh.transform_format = MultiMesh.TRANSFORM_3D
+	mmesh.use_custom_data = true
+	mmesh.mesh = mesh
+	
+	voxel_grid.multimesh = mmesh
+	
+	
+	region_size = calculate_region_size()
+
 	#If textures for animation are not provided, create them
 	if !color_texture or !position_offset_texture:
 		#Index 0 - color texure, index 1 - positions texture
@@ -61,8 +76,8 @@ func _ready() -> void:
 		color_texture = ImageTexture.create_from_image(images[0])
 		position_offset_texture = ImageTexture.create_from_image(images[1])
 		
-	#Width of the texture is used to calculate the number of cubes needed
-	#It corresponds to the highest number of solid pixels in all frames
+
+	#Width of the texture is used to calculate number of cubes
 	texture_width = color_texture.get_width()
 		
 	#Configure Multimesh
@@ -79,24 +94,49 @@ func _ready() -> void:
 	voxel_grid.material_override.shader = load("res://resources/vertex_animation_shader_m.gdshader")
 
 	voxel_grid.material_override.set_shader_parameter("colors", color_texture)
+	
 	voxel_grid.material_override.set_shader_parameter("positions", position_offset_texture)
 	voxel_grid.material_override.set_shader_parameter("voxel_size", voxel_size)
-	voxel_grid.material_override.set_shader_parameter("x_resolution", region_size.x)
-	voxel_grid.material_override.set_shader_parameter("y_resolution", region_size.y)
+	voxel_grid.material_override.set_shader_parameter("x_resolution", float(region_size.x))
+	voxel_grid.material_override.set_shader_parameter("y_resolution", float(region_size.y))
+	voxel_grid.material_override.set_shader_parameter("anim_frame", anim_frame)
 		
 	animations = animation_player.get_animation_list()
 	
 	#Remove the Reset animation from animation array
 	var idx = animations.find("RESET")
-	animations.remove_at(idx)
+	if idx > 0:
+		animations.remove_at(idx)
 	
 	current_animation = default_animation
 	
 	if current_animation:
 		change_anim_to(current_animation, fps / 10.0)
+		
+#Calculate the size of a single frame
+func calculate_region_size() -> Vector2:
+	return Vector2i(spritesheet.get_width() / h_frames, spritesheet.get_height() / v_frames)
 
 
-				
+#Change animation to a given one
+func change_anim_to(anim_name : StringName, speed_scale = 1.0):
+	animation_player.speed_scale = speed_scale
+	animation_player.play(anim_name)
+	current_animation = anim_name
+
+
+func next_anim():
+	if animations.size() < 2:
+		return
+	var idx : int = animations.find(current_animation)
+	change_anim_to(animations[(idx + 1) % animations.size()], fps / 10)
+
+func prev_anim():
+	if animations.size() < 2:
+		return
+	var idx : int = animations.find(current_animation)
+	change_anim_to(animations[(idx + 1) % animations.size()], fps / 10)
+
 #Create images for color and position offsets
 func create_images() -> Array[Image]:
 	var images : Array[Image] = []
@@ -109,7 +149,7 @@ func create_images() -> Array[Image]:
 	#Loop through all the frames
 	for y in v_frames:
 		for x in h_frames:
-			var region_rect : Rect2 = Rect2(Vector2(x * region_size.x, y * region_size.y), region_size)
+			var region_rect : Rect2i = Rect2i(Vector2i(x * region_size.x, y * region_size.y), region_size)
 			var frame_img : Image = spritesheet_image.get_region(region_rect)			
 			
 			var color_data : PackedByteArray = []
@@ -118,19 +158,22 @@ func create_images() -> Array[Image]:
 			
 			#Get raw frame data
 			var frame_data : PackedByteArray = frame_img.get_data()
-			
+
 			#Loop through pixels (4 bytes for 1 pixel)
 			#If the last byte is not equal to 0 (=solid pixel),
 			#Append the 4 bytes to color data
-			#And the position (calculated from pixel index in array) to position data		
-			for idx in range(0, frame_data.size(), 4):
-				if frame_data[idx + 3] != 0.0:
-					color_data.append_array(frame_data.slice(idx, idx + 4))
+			#And the position (calculated from pixel index in array) to position data
+
+			for idx in range(frame_data.size() - 1, -1, -4):
+				if frame_data[idx] != 0.0:
+
+					color_data.append_array(frame_data.slice(idx - 3, idx + 1))
 					pos_data.append_array([
-						(idx / 4) % region_size.x, (idx / 4) / region_size.x, 0, 255
+						(((idx - 3) / 4) % int(region_size.x)), (((idx - 3) / 4) / region_size.x), 0, 255
 					])
-		
+
 			pixel_count = pos_data.size()
+
 			color_array.append(color_data)
 			pos_array.append(pos_data)
 	
@@ -141,38 +184,26 @@ func create_images() -> Array[Image]:
 	#Fill the shorter arrays with 0s, to match the texture width
 	#Then create the image
 	var raw_data : PackedByteArray = []
-	var raw_data_pos : PackedByteArray = []
-	for i in color_array.size():
-		color_array[i].resize(longest_frame)
-		pos_array[i].resize(longest_frame)
-		raw_data.append_array(color_array[i])
-		raw_data_pos.append_array(pos_array[i])
-
+	for arr in color_array:
+		arr.resize(longest_frame)
+		raw_data.append_array(arr)
 
 	images.append(Image.create_from_data(longest_frame / 4, h_frames * v_frames, false, Image.FORMAT_RGBA8, raw_data))
-	images.append(Image.create_from_data(longest_frame / 4, h_frames * v_frames, false, Image.FORMAT_RGBA8, raw_data_pos))
+	
+	#Do the same for position values
+	raw_data = []
+	for arr in pos_array:
+		arr.resize(longest_frame)
+		raw_data.append_array(arr)
+	
+	images.append(Image.create_from_data(longest_frame / 4, h_frames * v_frames, false, Image.FORMAT_RGBA8, raw_data))
 	
 	return images
-		
-#Calculate the size of a single frame
-func calculate_region_size() -> Vector2i:
-	return Vector2i(spritesheet.get_width() / h_frames, spritesheet.get_height() / v_frames)
 
+func set_voxel_size(_voxel_size : Vector3):
+	voxel_size = _voxel_size
+	if is_instance_valid(voxel_grid.material_override):
+		voxel_grid.material_override.set_shader_parameter("voxel_size", voxel_size)
 
-func next_anim():
-	if animations.size() < 2:
-		return
-	var idx : int = animations.find(current_animation)
-	change_anim_to(animations[wrapi(idx + 1, 0, animations.size())], fps / 10)
-
-func prev_anim():
-	if animations.size() < 2:
-		return
-	var idx : int = animations.find(current_animation)
-	change_anim_to(animations[wrapi(idx - 1, 0, animations.size())], fps / 10)
-
-#Change animation to a given one
-func change_anim_to(anim_name : StringName, speed_scale = 1.0):
-	animation_player.speed_scale = speed_scale
-	animation_player.play(anim_name)
-	current_animation = anim_name
+func stop():
+	animation_player.stop()
